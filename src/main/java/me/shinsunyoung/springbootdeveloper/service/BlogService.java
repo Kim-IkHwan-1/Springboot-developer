7569f2b02a026329fd712f6c256e86b905d28100
keh70 <keh7055@naver.com>
2025-01-01 23:32:02 +0900

    변경

---
+++ src/main/java/me/shinsunyoung/springbootdeveloper/service/BlogService.java
@@ -0,0 +1,30 @@
+package me.shinsunyoung.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.shinsunyoung.springbootdeveloper.domain.Article;
+import me.shinsunyoung.springbootdeveloper.dto.AddArticleRequest;
+import me.shinsunyoung.springbootdeveloper.repository.BlogRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor // final이 붙거나 @NotNull이 붙은 필드의 생성자 후기
+@Service // 빈으로 등록
+public class BlogService {
+
+    private final BlogRepository blogRepository;
+
+    //블로그 글 추가 메소드
+    public Article save(AddArticleRequest request) {
+        return blogRepository.save(request.toEntity());
+    }
+
+    public List<Article> findAll() {
+        return blogRepository.findAll();
+    }
+
+    public Article findById(Long id) {
+        return blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+    }
+}
