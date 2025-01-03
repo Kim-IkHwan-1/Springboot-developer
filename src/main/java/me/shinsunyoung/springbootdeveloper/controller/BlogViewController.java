34213779b992864529b9a8e96ce3e5d095081e05
keh70 <keh7055@naver.com>
2025-01-03 22:06:39 +0900

    변경

--- src/main/java/me/shinsunyoung/springbootdeveloper/controller/BlogViewController.java
+++ src/main/java/me/shinsunyoung/springbootdeveloper/controller/BlogViewController.java
@@ -3,6 +3,7 @@ package me.shinsunyoung.springbootdeveloper.controller;
 import lombok.RequiredArgsConstructor;
 import me.shinsunyoung.springbootdeveloper.domain.Article;
 import me.shinsunyoung.springbootdeveloper.dto.ArticleListViewResponse;
+import me.shinsunyoung.springbootdeveloper.dto.ArticleViewResponse;
 import me.shinsunyoung.springbootdeveloper.service.BlogService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -34,5 +35,18 @@ public class BlogViewController {
         model.addAttribute("article", article);
 
         return "article";
+    }
+
+    @GetMapping("/new-article")
+    //id 키를 가진 쿼리 파라미터의 값을 id 변수에 매핑(id는 없을 수도 있음)
+    public String newArticle(@RequestParam(required = false) Long id, Model model){
+        if (id  == null) { //id 가 없으면 생성
+            model.addAttribute("article", new ArticleViewResponse());
+        }else { // id가 있으면 수정
+            Article article = blogService.findById(id);
+            model.addAttribute("article", new ArticleViewResponse(article));
+        }
+
+        return "newArticle";
     }
 }
